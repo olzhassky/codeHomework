7dc10634d1f7974950a426ebe51c3f7f299fb147
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-06-22 19:13:51 +0600

    Initial Commit

---
+++ codeHomework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codeHomework
+//
+//  Created by Olzhas Zhakan on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        
+    }
+
+
+}
+
